772971442360e00877f7cc9e8b96c2f7f673b59f
Hector <hectwilliams@gmail.com>
2024-04-14 12:45:58 -0400

    mnist successfully preprocessed and trained to keras model

--- Data/use_preprocess_mnist.py
+++ Data/use_preprocess_mnist.py
@@ -4,8 +4,6 @@
 import os 
 import keras 
 import preprocess.preprocess_mnist
-import sys 
-import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt 
 
@@ -16,22 +14,15 @@ OPTIMIZER= "sgd"
 MODEL_LOSS_= "sparse_categorical_crossentropy"
 MODEL_METRICS = ["accuracy"]
 
-rng = np.random.default_rng(seed=42)
-
 pre_process_mnist = preprocess.preprocess_mnist.PreprocessMnist(load_tf_records=True)
-(train_x, train_y), (test_x, test_y), (valid_x, valid_y) = pre_process_mnist.load_data()
-
-length_percent_25_train = int(0.25 * len(train_x)) 
-rand_indices = rng.integers(low =1, high=len(train_x), size=(length_percent_25_train))
-adapt_values = train_x[rand_indices]
-
-normalization = keras.layers.Normalization()
-normalization.adapt(adapt_values)
-normalized_train_x = normalization(train_x)
+train_feature, train_label = pre_process_mnist.load_dataset(dataset_name='training')
+validate_feature, validate_label = pre_process_mnist.load_dataset(dataset_name='validate')
+test_feature, test_label = pre_process_mnist.load_dataset(dataset_name='test')
 
 model = keras.models.Sequential(
             [
                 keras.layers.Input(shape=[28,28]),
+                pre_process_mnist.standardizer,
                 keras.layers.Flatten(),
                 keras.layers.Dense(300, activation=RELU_ACTIVATION),
                 keras.layers.Dense(200, activation=RELU_ACTIVATION),
@@ -40,15 +31,13 @@ model = keras.models.Sequential(
             ]
         )
 
-keras.utils.plot_model(model, os.path.join(CURR_DIR, __file__ [:-3]+ '.png'), show_shapes=True)
+keras.utils.plot_model(model, os.path.join(CURR_DIR, __file__ [:-3] + "_flow" + '.png'), show_shapes=True)
 model.compile(loss=MODEL_LOSS_, optimizer=OPTIMIZER, metrics=MODEL_METRICS)
-history = model.fit(normalized_train_x, train_y, validation_data=(valid_x, valid_y), epochs=30)
-print(model.summary())
-model.evaluate(test_x, test_y)
+history = model.fit(train_feature, train_label  ,epochs=5, validation_data=(validate_feature, validate_label))
+model.evaluate(test_feature, test_label)
 
+print(model.summary())
 pd.DataFrame(history.history).plot() 
 plt.grid(True)
 plt.gca().set_ylim(0,1)
-
-
-
+plt.savefig(  os.path.join(CURR_DIR, __file__ [:-3] + "_history" + '.png')   )
