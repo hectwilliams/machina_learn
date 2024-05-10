214deed16ee8f245316c8de61bbce9fbcf09c935
Hector <hectwilliams@gmail.com>
2024-05-09 21:18:43 -0400

    tf serving --> unable to test docker server: uninitialized error

--- serving/deploy.py
+++ serving/deploy.py
@@ -9,15 +9,56 @@ from some_model import MODEL_PATH, X_NEW
 import sys 
 import json 
 import requests 
+import grpc 
+from tensorflow_serving.apis.predict_pb2 import PredictRequest
+from tensorflow_serving.apis import prediction_service_pb2_grpc
+
+
+tf.__version__ # TensorFlow version: 2.16.1
+keras.__version__ # 3.1.1
 
 CURR_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__))) 
 LOAD_MODEL = False 
-SERVER_URL = 'http://localhost:8501/v1/models/my_model:predict'
+SERVER_URL = "http://localhost:8501/v1/models/my_model:predict" 
+VERSION_A = "0001"
+VERSION_B = "0002"
+MODEL_NAME = "my_model"
 
-headers = {"content-type": "application/json"}
-json_i = json.dumps({"signature_name": "serving_default", "instances": X_NEW[0:5].tolist() })
-print("response")
-# write json to file  TBD 
-json_response = requests.post(SERVER_URL, data=json_i, headers=headers)
-pred = json.loads( json_response.text )
-print(pred)
+os.environ["KERAS_BACKEND"] = "tensorflow"
+os.environ["MODEL_DIR"] = f"{MODEL_PATH}"
+
+tf.saved_model.load(MODEL_PATH)
+x = X_NEW[0:3]
+
+def gprc():
+    request = PredictRequest()
+    request.model_spec.name = MODEL_NAME
+    request.model_spec.signature_name = "serving_default"
+    x = x.astype('float32')
+    request.inputs['inputs'].CopyFrom(tf.make_tensor_proto(x))
+
+    channel = grpc.insecure_channel('localhost:8500')
+    service = prediction_service_pb2_grpc.PredictionServiceStub(channel)
+    resp = service.Predict(request)
+    sys.exit() 
+    proto = resp.outputs["output_0"]
+    print(proto)
+
+def rest():
+    headers = {"content-type": "application/json"}
+    x = (X_NEW[0:1])
+   
+    json_i = json.dumps(
+        {
+            "signature_name": "serving_default" ,
+            "instances": x.tolist(), 
+        }
+    )
+    print("response")
+    # write json to file  TBD 
+    json_response = requests.post(SERVER_URL, data=json_i, headers=headers)
+    pred = json.loads( json_response.text )
+    print(pred)
+    sys.exit()
+
+rest()
--- serving/some_model.py
+++ serving/some_model.py
@@ -1,34 +1,54 @@
 """Saves a model in SavedModels format"""
 
 import os 
-import tensorflow_datasets as tfds
 import keras 
-import sys 
 import tensorflow as tf
 import numpy as np 
 import sys
+import matplotlib.pyplot as plt
 
 CURR_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__))) 
 MODEL_PATH = os.path.abspath ( os.path.join(CURR_DIR, "../autoencoder"))
 sys.path.append(MODEL_PATH)
 import stacked_ae
 
-QUICK_PREDICT = False 
+CURR_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__))) 
 VERSION_A = "0001"
 VERSION_B = "0002"
 MODEL_NAME = "my_model"
 MODEL_PATH = os.path.join(CURR_DIR, MODEL_NAME, VERSION_A)
 LOSS = "binary_crossentropy"
 METRICS = [keras.metrics.Accuracy()] 
-OPTIMIZER = keras.optimizers.SGD(learning_rate=0.001) 
+OPTIMIZER = keras.optimizers.SGD(learning_rate=1.5)
 X_NEW = stacked_ae.x_valid 
 X_NEW_TRAIN = stacked_ae.x_train
 
-# np.save( os.path.join(CURR_DIR , "my_tests.npy") , X_NEW )
+
+
+# predict 
+x = X_NEW[0]
+x_new_local = X_NEW[0:3]
 
 if __name__ == "__main__":
-    model = stacked_ae.stacked_autoencoder_model
-    model.compile(loss=LOSS, optimizer=OPTIMIZER)
-    model.fit(X_NEW_TRAIN, X_NEW_TRAIN, epochs=5)
-    print(model.get_weights())
-    tf.saved_model.save(model, MODEL_PATH)
+    
+    if os.path.isdir(MODEL_PATH):
+        np.save( os.path.join(  CURR_DIR , "my_tests.npy"), x_new_local)
+        imported = tf.saved_model.load(MODEL_PATH)
+        model = imported.signatures["serving_default"]
+        pred = model(  tf.constant (x,  dtype=tf.float32) )
+        pred = pred['output_0'].numpy()
+        plt.imshow(pred[0], cmap="binary")
+        plt.savefig(os.path.join(CURR_DIR, __file__ [:-3] + '_visualize_' +'.png'))
+
+    else: 
+        model = keras.models.clone_model (stacked_ae.stacked_autoencoder_model)
+        model.compile(loss=LOSS, optimizer=OPTIMIZER, metrics=[keras.metrics.BinaryAccuracy()] )
+        model.fit(X_NEW_TRAIN, X_NEW_TRAIN, epochs=1)
+        model.evaluate(stacked_ae.x_valid, stacked_ae.x_valid)
+        print(model.get_weights())
+        model.summary()
+        tf.saved_model.save(
+            model, 
+            MODEL_PATH, 
+        )
+        # model.save(MODEL_PATH)
